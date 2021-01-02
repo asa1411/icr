919dc29da303a2653a4350f218f3cbad9df3ed0c
asa1411 <45476911+asa1411@users.noreply.github.com>
2021-01-02 08:56:24 -0600

    new trial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace icr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
